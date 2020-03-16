616f1df46d5ec288c2118a97a4e5240ab34f7458
joshua.alley-at-961699485160 <jauschalley@gmail.com>
2020-03-16 16:30:29 -0400

    fixing eslint

---
+++ ims-react/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "globals": {
+        "$": true,
+        "require": true,
+        "process": true,
+        "expect": true,
+        "it": true,
+        "describe": true
+      },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
